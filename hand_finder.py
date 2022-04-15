246e9516a9d7620e2769389c3dbc1ff6b26f27f6
Sahaj Patel <sah4j.patel@gmail.com>
2022-04-15 19:29:05 -0400

    q to quit

--- hand_finder.py
+++ hand_finder.py
@@ -17,6 +17,7 @@ import mediapipe
 import cv2
 import time
 import copy
+import keyboard
 
 
 def num_to_letter(numbers):
@@ -67,37 +68,42 @@ def num_to_letter(numbers):
 
 model = keras.models.load_model("CNN_model.h5")
 
+
 cap = cv2.VideoCapture(0)
 detector = HandDetector(detectionCon=0.8, maxHands=1)
 time_curr = time.time()
-while True:
+while not keyboard.is_pressed("q"):
     __s, img = cap.read()
     tmp = copy.copy(img)
     hand, imgP = detector.findHands(img)
-    cv2.imshow("R", tmp)
 
     if hand and len(hand) == 1 and time.time() >= time_curr + 1:
         img2 = tmp[
-            hand[0]["bbox"][1] - 20
-            if hand[0]["bbox"][1] > 20
-            else hand[0]["bbox"][1] : hand[0]["bbox"][1] + hand[0]["bbox"][3] + 20
-            if hand[0]["bbox"][1] + hand[0]["bbox"][3] + 20 < img.shape[0]
+            hand[0]["bbox"][1] - 50
+            if hand[0]["bbox"][1] > 50
+            else hand[0]["bbox"][1] : hand[0]["bbox"][1] + hand[0]["bbox"][3] + 50
+            if hand[0]["bbox"][1] + hand[0]["bbox"][3] + 50 < img.shape[0]
             else hand[0]["bbox"][1] + hand[0]["bbox"][3],
-            hand[0]["bbox"][0] - 20
-            if hand[0]["bbox"][0] > 20
-            else hand[0]["bbox"][1] : hand[0]["bbox"][0] + hand[0]["bbox"][2] + 20
-            if hand[0]["bbox"][0] + hand[0]["bbox"][2] + 20 < img.shape[1]
+            hand[0]["bbox"][0] - 50
+            if hand[0]["bbox"][0] > 50
+            else hand[0]["bbox"][1] : hand[0]["bbox"][0] + hand[0]["bbox"][2] + 50
+            if hand[0]["bbox"][0] + hand[0]["bbox"][2] + 50 < img.shape[1]
             else hand[0]["bbox"][0] + hand[0]["bbox"][2],
         ]
-        final_img = cv2.resize(img2, (64, 64))
-        final_img = cv2.cvtColor(final_img, cv2.COLOR_BGR2RGB)
-        final_img = np.expand_dims(final_img, axis=0)
-        cv2.imshow("Result", img2)
-        pred = model.predict(final_img)
-        test_val = np.argmax(pred, axis=1)
-        print(num_to_letter(test_val))
-        time_curr = time.time()
+        try:
+            final_img = cv2.resize(img2, (64, 64))
+            final_img = cv2.cvtColor(final_img, cv2.COLOR_BGR2RGB)
+            final_img = np.expand_dims(final_img, axis=0)
+            cv2.imshow("Result", img2)
+            pred = model.predict(final_img)
+            test_val = np.argmax(pred, axis=1)
+            print(num_to_letter(test_val))
+            time_curr = time.time()
+        except:
+            pass
+
     cv2.imshow("Image", imgP)
     cv2.waitKey(1)
+
 cap.release()
 cv2.destroyAllWindows()
